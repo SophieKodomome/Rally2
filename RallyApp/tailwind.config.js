24ca7fcfbb21753c8807fb985a66061b7693df9c
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-05-19 10:23:42 +0300

    Tailwind added

---
+++ RallyApp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.{cshtml,html}',
+    './Views/**/*.{cshtml,html}',
+    './wwwroot/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
