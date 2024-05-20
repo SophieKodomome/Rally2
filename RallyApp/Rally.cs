6541714ac10b9ca241b8196f7f6245f49f00dde1
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-05-20 12:08:37 +0300

    Create Rally.cs

---
+++ RallyApp/Rally.cs
@@ -0,0 +1,20 @@
+namespace race
+{
+    public class Rally
+    {
+        public int Number{get;set;}
+        List<Racer> ListRacer{get;set;}
+
+        public Rally AddNumber(int n)
+        {
+            Number=n;
+            return this;
+        }
+
+        public Rally AddListRacer(List<Racer> lr)
+        {
+            ListRacer=lr;
+            return this;
+        }
+    }    
+}
